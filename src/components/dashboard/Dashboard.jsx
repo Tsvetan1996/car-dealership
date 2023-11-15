c4a89f71a5d893d2430bd875e256ba11b60900cd
Tsvetan Atanasov <85298268+Tsvetan1996@users.noreply.github.com>
2023-11-15 22:24:58 +0200

    Add temp dashboard item with basic styling

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -3,8 +3,31 @@ import styles from "./Dashboard.module.css";
 export default function Dashboard() {
   return (
     <div className={styles.dashboardContainer}>
-      <h2>Dashboard</h2>
-      <p>Welcome to your dashboard! Here's some placeholder content.</p>
+      <div className={styles.productCard}>
+        <img
+          className={styles.productImage}
+          src="../../public/rs7.png"
+          alt="Product"
+        />
+        <div className={styles.productInfo}>
+          <h3>Audi</h3>
+          <p>RS7</p>
+          <button className={styles.detailsButton}>Details</button>
+        </div>
+      </div>
+
+      <div className={styles.productCard}>
+        <img
+          className={styles.productImage}
+          src="../../public/rs7.png"
+          alt="Product"
+        />
+        <div className={styles.productInfo}>
+          <h3>Audi</h3>
+          <p>RS7</p>
+          <button className={styles.detailsButton}>Details</button>
+        </div>
+      </div>
     </div>
   );
 }
